02e48dfcfbdb449e8d9417e29f9467565df95169
Dennis Kool <43271994+dkooll@users.noreply.github.com>
2023-02-14 14:06:43 +0100

    feat: add documentation (#4)

--- main.tf
+++ main.tf
@@ -41,7 +41,7 @@ resource "random_string" "random" {
 # virtual machine scale set
 #----------------------------------------------------------------------------------------
 
-resource "azurerm_linux_virtual_machine_scale_set" "main" {
+resource "azurerm_linux_virtual_machine_scale_set" "vmss" {
   name                = "vmss-${var.company}-${random_string.random.result}"
   resource_group_name = var.vmss.resource_group
   location            = var.vmss.location
