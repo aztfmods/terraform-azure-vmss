03d434f0e83e14f22ca5b40eaa66d5426dca4259
Dennis Kool <43271994+dkooll@users.noreply.github.com>
2023-02-14 14:12:22 +0100

    feat: add validation workflow (#5)

---
+++ tests/vmss_test.go
@@ -0,0 +1,29 @@
+package test
+
+import (
+	"testing"
+
+	"github.com/gruntwork-io/terratest/modules/terraform"
+)
+
+func TestApplyNoError(t *testing.T) {
+	t.Parallel()
+
+	tests := []string{
+		"../examples/simple",
+	}
+
+	for _, test := range tests {
+		t.Run(test, func(t *testing.T) {
+			terraformOptions := &terraform.Options{
+				TerraformDir: test,
+				NoColor:      true,
+			}
+
+			terraform.WithDefaultRetryableErrors(t, &terraform.Options{})
+
+			defer terraform.Destroy(t, terraformOptions)
+			terraform.InitAndApply(t, terraformOptions)
+		})
+	}
+}
